10620335437f6d13a8fcf5ac8313a48b80a4ef68
Halftonex <nikook13@gmail.com>
2024-07-05 16:23:12 +0200

    x

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/[calendario-poghetin]/",
+  base: "/calendario-poghetin/",
   plugins: [react()],
 });
